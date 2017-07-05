d2852aa9017c57f44af8d145104c349a69b323db
shunko7 <shunko7@users.noreply.github.com>
2017-07-05 15:21:10 +0200

    hotfix

--- includes/oLogin.php
+++ includes/oLogin.php
@@ -1,10 +1,9 @@
 <?php
 include "dbconnect.php";
 
-// User-Login
-$ifError = false;
+// Admin-Login
 
-if(isset($_POST['oName'])&&isset($_POST['oPassword'])){
+if(isset($_POST['oName'])&&isset($_POST['oPassword'])) {
 
     $oName = mysqli_real_escape_string($pdo, $_POST['oName']);
 
@@ -13,27 +12,22 @@ if(isset($_POST['oName'])&&isset($_POST['oPassword'])){
     $sql = "SELECT `organizerID`,`password` FROM `organizer` WHERE `organizerName` = '$oName'";
     $result = mysqli_query($pdo, $sql);
 
-    if(!$result) {
+    if (!$result) {
         echo mysqli_error($pdo);
     }
 
-    if(mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
         $passwordFromDb = $row['password'];
 
-        if(password_verify($plainTextPassword, $passwordFromDb)) {
+        if (password_verify($plainTextPassword, $passwordFromDb)) {
             $_SESSION['login_user'] = $oName;
             $_SESSION['login_userid'] = $row['organizerID'];
 
             header("location: application.php");
 
-        } else {
-            $ifError = true;
         }
 
-    } else {
-        $ifError = true;
     }
-
 }
